314da5665cf1ce7a1f167875a624b61fa8593080
Mizael Santos <mizael.santos@accenture.com>
2019-04-07 20:14:02 -0300

    amend code

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,19 @@
-width(60, document.getElementsByTagName("width")[0])
-width(50, document.getElementsByTagName("width")[1])
-width(60, document.getElementsByTagName("width")[2])
-width(50, document.getElementsByTagName("width")[3])
-width(60, document.getElementsByTagName("width")[4])
-width(30, document.getElementsByTagName("width")[5])
-width(40, document.getElementsByTagName("width")[6])
-width(70, document.getElementsByTagName("width")[7])
-width(60, document.getElementsByTagName("width")[8])
-width(40, document.getElementsByTagName("width")[9])
-width(50, document.getElementsByTagName("width")[10])
-width(90, document.getElementsByTagName("width")[11])
-width(40, document.getElementsByTagName("width")[12])
+const values = document.getElementsByClassName("width");
+
+for (let i = 0; i < values.length; i++) {
+  width(values[i].getAttribute('data-percent'), values[i]);
+}
 
 function width(size, elem) {
-    var width = 0;
-    var id = setInterval(frame, 10);
-    function frame() {
-      if (width == size) {
-        clearInterval(id);
-      } else {
-        width++; 
-        elem.style.width = width + '%'; 
-      }
+  var width = 0;
+  var id = setInterval(frame, 10);
+
+  function frame() {
+    if (width == size) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
     }
   }
+}
